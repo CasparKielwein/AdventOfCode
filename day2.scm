f4cb3a06f88bb566e716b4e41473c581865f514d
Caspar Kielwein <c.kielwein@bretgeld-engineering.de>
2022-12-06 22:02:03 +0100

    part2 of day2

--- day2.scm
+++ day2.scm
@@ -45,7 +45,24 @@
     ((equal? match '("C" "Y")) 0)
     ((equal? match '("C" "Z")) 3)))
 
-
+;; calcuate the full score of the input set for part 1
 (+
   (sum (raw-points rounds))
   (sum (map result rounds)))
+
+  
+;; calculate the full value of what we need to chose
+;; X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+(define (result match)
+  (cond
+    ((equal? match '("A" "X")) (+ 6 1))
+    ((equal? match '("A" "Y")) (+ 3 1))
+    ((equal? match '("A" "Z")) (+ 0 1))
+    
+    ((equal? match '("B" "X")) (+ 6 2))
+    ((equal? match '("B" "Y")) (+ 3 2))
+    ((equal? match '("B" "Z")) (+ 0 2))
+    
+    ((equal? match '("C" "X")) (+ 6 3))
+    ((equal? match '("C" "Y")) (+ 3 3))
+    ((equal? match '("C" "Z")) (+ 0 3))))
